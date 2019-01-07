2d0b2c48c2165b252ae95a1ce60feb5256a9c3bb
phitranpt <phi.tran.pt@gmail.com>
2019-01-06 21:17:50 -0600

    added tabs, additional questions with radios to form

--- src/components/Auto/Auto.js
+++ src/components/Auto/Auto.js
@@ -3,41 +3,70 @@ import { connect } from 'react-redux';
 
 import propTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
-import { Paper, Typography } from '@material-ui/core';
+import { 
+  Paper, 
+  Typography,
+  Tab,
+  Tabs,
+} from '@material-ui/core';
 import AutoForm from './AutoForm';
+import Person from '@material-ui/icons/Person';
+import DirectionsCar from '@material-ui/icons/DirectionsCar';
 
 const styles = theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
   paper: {
-    ...theme.mixins.gutters(),
-    paddingTop: theme.spacing.unit * 10,
-    paddingBottom: theme.spacing.unit * 10,
+    paddingRight: 80,
+    paddingLeft: 80,
+    paddingBottom: 40,
+    paddingTop: 40,
     width: 700,
-    marginTop: '5vh',
     margin: 'auto',
-  }
+    marginBottom: '5vh',
+    borderRadius: '0px 0px 4px 4px',
+    boxShadow: 'none',
+  },
+  tabs: {
+    width: 700,
+    margin: 'auto',
+    borderRadius: '4px 4px 0px 0px',
+    borderBottom: '1px solid grey',
+    boxShadow: 'none',
+  },
 });
 
 class Auto extends Component {
 
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
     const { classes } = this.props;
       return (
-        <div className={classes.root}>
+        <div>
+          <Paper className={classes.tabs}>
+            <Tabs
+                value={this.state.value}
+                onChange={this.handleChange}
+                variant="fullWidth"
+                indicatorColor="primary"
+                textColor="primary"
+              >
+              <Tab icon={<DirectionsCar />} label="VEHICLE" />
+              <Tab icon={<Person />} label="DRIVER" />
+            </Tabs>
+          </Paper>
+
           <Paper className={classes.paper} elevation={1}>
-            <Typography variant="h5" component="h3">
+            <Typography className={classes.text} variant="h5" component="h3">
               Please tell us a little more about your vehicle.
             </Typography>
-            <br></br>
             <Typography component="p">
               Let us know your info. The more accurate it is, the more accurate your quote will be. We'll send your information over to the most appropriate agent.
-            </Typography>
-            <br></br>
-            <Typography component="p">
-              What vehicle would you like to insure?
             </Typography>
             <br></br>
             <AutoForm/>
--- src/components/Auto/AutoForm.js
+++ src/components/Auto/AutoForm.js
@@ -10,23 +10,33 @@ import {
     InputLabel,
     FormControl,
     Select,
-    TextField} from '@material-ui/core';
+    TextField, 
+    StepLabel,
+    RadioGroup,
+    FormControlLabel,
+    Radio,
+} from '@material-ui/core';
 
 const styles = theme => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        width: 600,
-        background: '',
-      },
-      formControl: {
-        minWidth: 260,
-        height: 65,
-        margin: 6,
-      },
-      selectEmpty: {
-        marginTop: theme.spacing.unit * 2,
-      },
+    formControl: {
+        width: '48%',
+    },
+    formControlTwo: {
+        width: '48%',
+        float: 'right',
+    },
+    miles: {
+        width: '100%'
+    },
+    numYearForm: {
+        width: '100%',
+    },
+    stepLabel: {
+        marginTop: 30,
+    },
+    radio: {
+        display: 'inline'
+    },
 });
 
 class AutoForm extends Component {
@@ -36,6 +46,9 @@ class AutoForm extends Component {
         make: '',
         model: '',
         style: '',
+        numYear: '',
+        finance: '',
+        miles: '',
       };
     
       componentDidMount() {
@@ -51,8 +64,10 @@ class AutoForm extends Component {
   render() {
     const { classes } = this.props;
       return (
-        <form className={classes.root}>
-            <FormControl variant="outlined" className={classes.formControl}>
+        <div>
+
+            <StepLabel className={classes.stepLabel}>What vehicle would you like to insure?</StepLabel>
+            <FormControl variant="outlined" margin="normal" className={classes.formControl}>
                 <InputLabel
                     ref={ref => {
                     this.InputLabelRef = ref;
@@ -101,13 +116,13 @@ class AutoForm extends Component {
                 required
                 id="outlined-required"
                 label="Make"
-                className={classes.formControl}
+                className={classes.formControlTwo}
                 margin="normal"
                 variant="outlined"
                 value={this.state.make}
                 onChange={this.handleChange('make')}
             />
-
+            
             <TextField
                 required
                 id="outlined-required"
@@ -123,13 +138,129 @@ class AutoForm extends Component {
                 required
                 id="outlined-required"
                 label="Style"
-                className={classes.formControl}
+                className={classes.formControlTwo}
                 margin="normal"
                 variant="outlined"
                 value={this.state.style}
                 onChange={this.handleChange('style')}
             />
-        </form>
+
+            <StepLabel className={classes.stepLabel}>How many years have you had this vehicle?</StepLabel>
+            <FormControl variant="outlined" margin="normal" className={classes.numYearForm}>
+                <InputLabel
+                    ref={ref => {
+                        this.InputLabelRef = ref;
+                        }}
+                        htmlFor="outlined-numYear-native-simple"
+                >
+                    Please select number of years
+                </InputLabel>
+                <Select
+                    native
+                    value={this.state.numYear}
+                    onChange={this.handleChange('numYear')}
+                    input={
+                    <OutlinedInput
+                        name="numYear"
+                        labelWidth={this.state.labelWidth}
+                        id="outlined-numYear-native-simple"
+                    />
+                    }
+                >
+                    <option value="" />
+                    <option value={0-1}>Less than 1 year</option>
+                    <option value={1}>1 years</option>
+                    <option value={2}>2 years</option>
+                    <option value={3}>3 years</option>
+                    <option value={4}>4 years</option>
+                    <option value={5}>5 years</option>
+                    <option value={6}>6 years</option>
+                    <option value={7}>7 years</option>
+                    <option value={8}>8 years</option>
+                    <option value={9}>9 years</option>
+                    <option value={10}>10+ years</option>
+                </Select>
+            </FormControl>
+                
+            <StepLabel className={classes.stepLabel}>Do you own, lease, or finance this vehicle?</StepLabel>
+            <RadioGroup
+                name="finance"
+                className={classes.radio}
+                value={this.state.finance}
+                onChange={this.handleChange('finance')}
+            >
+                <FormControlLabel value="Own" control={<Radio />} label="Own" />
+                <FormControlLabel value="Lease" control={<Radio />} label="Lease" />
+                <FormControlLabel value="Finance" control={<Radio />} label="Finance" />
+            </RadioGroup>
+
+            <StepLabel className={classes.stepLabel}>What is the main purpose of this vehicle?</StepLabel>
+            <RadioGroup
+                name="usage"
+                className={classes.radio}
+                value={this.state.usage}
+                onChange={this.handleChange('usage')}
+            >
+                <FormControlLabel value="Commute" control={<Radio />} label="Commute" />
+                <FormControlLabel value="Pleasure" control={<Radio />} label="Pleasure" />
+                <FormControlLabel value="Business" control={<Radio />} label="Business" />
+            </RadioGroup>
+
+            <StepLabel className={classes.stepLabel}>How many miles do you drive this vehicle annually?</StepLabel>
+            <FormControl variant="outlined" margin="normal" className={classes.miles}>
+                <InputLabel
+                    ref={ref => {
+                        this.InputLabelRef = ref;
+                        }}
+                        htmlFor="outlined-miles-native-simple"
+                >
+                    Please select number of miles
+                </InputLabel>
+                <Select
+                    native
+                    value={this.state.miles}
+                    onChange={this.handleChange('miles')}
+                    input={
+                    <OutlinedInput
+                        name="miles"
+                        labelWidth={this.state.labelWidth}
+                        id="outlined-miles-native-simple"
+                    />
+                    }
+                >
+                    <option value="" />
+                    <option value={0-4999}>0-4999 miles</option>
+                    <option value={5000-9999}>5000-9999 miles</option>
+                    <option value={10000-14999}>10,000-14,999 miles</option>
+                    <option value={15000-19999}>15,000-19,999 miles</option>
+                    <option value={20000-24999}>20,000-24,999 miles</option>
+                    <option value={25000-29999}>25,000-29,999 miles</option>
+                    <option value={30000}>30,000+ miles</option>
+                </Select>
+            </FormControl>
+
+            <StepLabel className={classes.stepLabel}>Does this vehicle have an anti-theft locking device?</StepLabel>
+            <RadioGroup
+                name="theft"
+                className={classes.radio}
+                value={this.state.theft}
+                onChange={this.handleChange('theft')}
+            >
+                <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+                <FormControlLabel value="No" control={<Radio />} label="No" />
+            </RadioGroup>
+
+            <StepLabel className={classes.stepLabel}>Do you keep this vehicle at your home address?</StepLabel>
+            <RadioGroup
+                name="theft"
+                className={classes.radio}
+                value={this.state.theft}
+                onChange={this.handleChange('theft')}
+            >
+                <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+                <FormControlLabel value="No" control={<Radio />} label="No" />
+            </RadioGroup>
+        </div>
       );
   }
 }
