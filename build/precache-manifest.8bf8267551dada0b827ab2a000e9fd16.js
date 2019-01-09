c796948d68ec074cc8243b12b62720d2d17e8a29
phitranpt <phi.tran.pt@gmail.com>
2019-01-08 20:29:13 -0600

    initial commit to heroku

---
+++ build/precache-manifest.8bf8267551dada0b827ab2a000e9fd16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b75e8b8285dfda7bb46b",
+    "url": "/static/js/main.b75e8b82.chunk.js"
+  },
+  {
+    "revision": "cf79010f8d4b8a4ad9a3",
+    "url": "/static/js/1.cf79010f.chunk.js"
+  },
+  {
+    "revision": "b75e8b8285dfda7bb46b",
+    "url": "/static/css/main.c89da090.chunk.css"
+  },
+  {
+    "revision": "d2a62807d7d02ad634f85d8d5c1c54cd",
+    "url": "/index.html"
+  }
+];
