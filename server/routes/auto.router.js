a5474c3e0cdf72fb626c3380f1b2d4e18a644404
phitranpt <phi.tran.pt@gmail.com>
2019-01-26 18:10:52 -0600

    added auto router to connect client to database. posted first route

--- server/routes/auto.router.js
+++ server/routes/auto.router.js
@@ -7,7 +7,21 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-
+    console.log('in auto router', req.body);
+    const autoForm = req.body;
+    const queryValues = [
+        autoForm.firstName,
+        autoForm.lastName,
+    ]
+    const sqlText = `INSERT INTO contact ("first_name, last_name") VALUES ($1, $2);`;
+    pool.query(sqlText, queryValues)
+    .then(() => {
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log('error with autoform post router', error);
+        res.sendStatus(500)
+    })
 });
 
 module.exports = router;
